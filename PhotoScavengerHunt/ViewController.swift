66e59b30bb88049cabd48ecd9ba40cc78c328ca5
kkellybonilla <kkelly.bonilla@stanford.edu>
2023-03-14 03:40:00 -0400

    Initial Commit

---
+++ PhotoScavengerHunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoScavengerHunt
+//
+//  Created by Kelly Bonilla Guzmán on 3/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
